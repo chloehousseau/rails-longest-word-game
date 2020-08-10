868679da0e4f57c9b13c2a1a315294125809c410
chloe housseau <chloe.housseau@gmail.com>
2020-08-10 17:05:51 +0200

    rails longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ('a'..'z').to_a
+    @letters = []
+    10.times { @letters << alphabet.sample }
+  end
+
+  def score
+  end
+end
